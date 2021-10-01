082eff987ffea9c7c53ecef1219e026f7f634d8e
alexbalak21 <alex.balak@outlook.com>
2021-10-01 18:41:17 +0200

    Product page done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-var menuItems = document.getElementById("menuItems");
+let menuItems = document.getElementById("menuItems");
 menuItems.style.maxHeight = "0px";
 function menuToggle() {
   if (menuItems.style.maxHeight == "0px") menuItems.style.maxHeight = "230px";
   else menuItems.style.maxHeight = "0px";
+}
+
+let mainImg = document.getElementById("mainImg");
+let imgLink = document.getElementById("imgLink");
+
+function mainImgCh(img) {
+  mainImg.src = `assets/img/gallery-${img}.jpg`;
+  imgLink.setAttribute("href", `assets/img/gallery-${img}.jpg`);
 }
