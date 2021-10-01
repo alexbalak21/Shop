4cb9d4dfaf51745b71bbf150dbad38fb6144ab3f
alexbalak21 <alex.balak@outlook.com>
2021-10-01 14:43:20 +0200

    Home page done an updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var menuItems = document.getElementById("menuItems");
 menuItems.style.maxHeight = "0px";
 function menuToggle() {
-  if (menuItems.style.maxHeight == "0px") menuItems.style.maxHeight = "200px";
+  if (menuItems.style.maxHeight == "0px") menuItems.style.maxHeight = "230px";
   else menuItems.style.maxHeight = "0px";
 }
