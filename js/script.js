52a9f7965f1e44197db0fa98a9cadcda92c16ba9
alexbalak21 <alex.balak@outlook.com>
2021-10-02 13:41:41 +0200

    registration form and login form done

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@ function mainImgCh(img) {
   mainImg.src = `assets/img/gallery-${img}.jpg`;
   imgLink.setAttribute("href", `assets/img/gallery-${img}.jpg`);
 }
+
+// TOGGLE FROM
+
+let loginFrom = document.getElementById("loginForm");
+let regFrom = document.getElementById("regForm");
+let indicator = document.getElementById("indicator");
+
+function regDisp() {
+  regFrom.style.transform = "translateX(0px)";
+  loginFrom.style.transform = "translateX(0px)";
+  indicator.style.transform = "translateX(108px)";
+}
+
+function loginDisp() {
+  regFrom.style.transform = "translateX(300px)";
+  loginFrom.style.transform = "translateX(300px)";
+  indicator.style.transform = "translateX(0px)";
+}
